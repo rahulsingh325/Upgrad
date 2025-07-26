33bf7b7686f55a0cc4e5e7cecff56b8f1692301b
rahul <therahulsingh325@gmail.com>
2025-07-26 19:36:32 +0530

    this is the improvise version  after reviewing once

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,26 @@
+
 window.onload = function () {
     const cards = document.querySelectorAll(".card-item");
+    const image = document.getElementById("chart-image");
 
     cards.forEach((card) => {
         card.addEventListener("click", () => {
+            // Remove 'active' class from all cards
             cards.forEach((c) => c.classList.remove("active"));
+
+            // Add 'active' to clicked one
             card.classList.add("active");
+
+            // Change image
+            const imageSrc = card.getAttribute("data-image");
+            if (imageSrc) {
+                image.src = imageSrc;
+            }
         });
     });
 };
+
+
 
 const buttons = document.querySelectorAll('.button');
 
@@ -15,7 +28,7 @@ buttons.forEach(button => {
     button.addEventListener('click', () => {
         Swal.fire({
             title: '🎉 Button Clicked!',
-            text: 'You just clicked a button.', 
+            text: 'You just clicked a button.',
             icon: 'success',
             confirmButtonText: 'Got it!',
             confirmButtonColor: '#3085d6',
